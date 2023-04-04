2d16a40a0844dde245ce55c57f3ac0acab3a7fa5
dti <dti@LAB204-16>
2023-04-04 11:20:53 -0300

    terceira_ediçao

---
+++ sig1/src/main/java/com/fatec/sig1/model/MantemOngRepository.java
@@ -0,0 +1,27 @@
+package com.fatec.sig1.model;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+/**
+ * Esta classe abstrai a programação de consultas para acesso ao banco de dados.
+ * O nome dos metodos para consulta têm duas partes separadas pela palavra-chave
+ * By. A primeira parte é o “find” seguido de By. A segunda parte é o nome do
+ * atributo na tabela por exemplo Cpf - findByCpf
+ * 
+ * @author
+ */
+@Repository
+public interface MantemOngRepository extends JpaRepository<Ong, Long> {
+    
+    Optional<Ong> findByCNPJ(String cnpj);
+
+    List<Ong> findAllByNomeIgnoreCaseContaining(String nome);
+
+    Optional<Ong> findBySegmento(String segmento);
+  
+}
---
+++ sig1/src/main/java/com/fatec/sig1/model/Ong.java
@@ -0,0 +1,142 @@
+package com.fatec.sig1.model;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.br.CNPJ;
+import org.joda.time.DateTime;
+
+//The JPA was renamed as Jakarta Persistence in 2019 and version 3.0 was released in 2020. This included the renaming of packages and properties
+//from javax. persistence to jakarta. persistence.
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Ong {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotBlank(message = "Nome é requerido")
+	private String nome;
+	
+	@NotBlank(message = "Insira apenas números")
+	private int telefone;
+	
+	@CNPJ
+	@NotBlank(message = "O CNPJ é obrigatório")
+	@Column(unique = true) // nao funciona com @Valid tem que tratar na camada de persistencia
+	private String cnpj;
+	
+	@NotBlank(message = "O CEP é obrigatório.")
+	private String cep;
+	
+	private String endereco;
+	@NotBlank(message = "O complemento deve ser informado")
+	
+	private String complemento;
+	private String descricao;
+	private String segmento;
+	
+	@Pattern(regexp = "/^[a-z0-9.]+@[a-z0-9]+\\.[a-z]+\\.([a-z]+)?$/i", message = "O e-mail deve ser escrito no formato nome@gmail.com")
+	@NotBlank(message = "O Email é obrigatório")
+	@Column(unique = true) // nao funciona com @Valid tem que tratar na camada de persistencia
+	private String email;
+
+	@NotBlank(message = "A senha é obrigatório")
+	private String senha;
+	
+	
+	public Ong(String nome, int telefone, String cep, String endereco, String complemento, String descricao, String segmento, String email, String senha, @CNPJ @NotBlank(message = "O CNPJ é obrigatório") String cnpj) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.cnpj = cnpj;
+		this.cep = cep;
+		this.endereco = endereco;
+		this.complemento = complemento;
+		this.descricao = descricao;
+		this.segmento = segmento;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Ong() {
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(int telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getSegmento() {
+		return segmento;
+	}
+
+	public void setSegmento(String segmento) {
+		this.segmento = segmento;
+	}
+	
+}
+	
+	
